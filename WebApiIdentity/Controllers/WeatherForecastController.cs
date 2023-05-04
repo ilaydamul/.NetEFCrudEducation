d38e60523df09495a1e96b1b3e035fcffbc6b161
ilaydamul <Ilayda2003.1905@hotmail.com>
2023-05-04 15:18:48 +0300

    API Start

---
+++ WebApiIdentity/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiIdentity.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        // api/users/1/profile
+        // api/User/Profile/1 --MVC
+        [HttpGet("list",Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("getBy")]
+        public IActionResult GetById([FromQuery] string id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("id")]
+        public IActionResult Test(string id)
+        {
+            return Ok();
+        }
+    }
+}
